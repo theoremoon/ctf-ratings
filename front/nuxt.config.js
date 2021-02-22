546dc986b1f39b6317e038ebeb8b2be68b44735b
theoremoon <theoldmoon0602@gmail.com>
2021-02-22 10:17:06 +0900

    update nuxt config

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -2,5 +2,15 @@ export default {
     components: true,
     head: {
         titleTemplate: 'CTF-Ratings'
+    },
+    target: 'static',
+    ssr: false,
+    generate: {
+        exclude: [
+            /^\/team/
+        ]
+    },
+    router: {
+        base: '/ctf-ratings/'
     }
 }
