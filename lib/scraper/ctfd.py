87e48b8d0f3b46ac0e71082b9e689d45d9cf1b9e
theoremoon <theoldmoon0602@gmail.com>
2021-04-12 10:35:55 +0900

    RITSEC CTF 2021

--- lib/scraper/ctfd.py
+++ lib/scraper/ctfd.py
@@ -5,6 +5,7 @@ from urllib.parse import urljoin
 from datetime import datetime
 from logging import getLogger, StreamHandler, Formatter
 from tqdm import tqdm
+from http.cookies import SimpleCookie
 
 logger = getLogger(__name__)
 handler = StreamHandler()
@@ -14,8 +15,13 @@ logger.addHandler(handler)
 class CTFdScraper():
     def __init__(self, url, **kwargs):
         self.url = url
-        if "session" in kwargs:
-            self.session = kwargs["session"]
+
+        cookies = SimpleCookie()
+        if "cookies" in kwargs and kwargs["cookies"]:
+            cookies.load(kwargs["cookies"])
+
+        self.cookiejar = requests.cookies.RequestsCookieJar()
+        self.cookiejar.update(cookies)
 
         self.mode = 'teams'
         if 'mode' in kwargs:
@@ -26,10 +32,7 @@ class CTFdScraper():
         retries = Retry(total=5, backoff_factor=0.1, status_forcelist=[500, 501, 502, 503, 504])
         s.mount("http://", HTTPAdapter(max_retries=retries))
         s.mount("https://", HTTPAdapter(max_retries=retries))
-        if self.session:
-            return s.get(urljoin(self.url, path), cookies={'session': self.session})
-        else:
-            return s.get(urljoin(self.url, path))
+        return s.get(urljoin(self.url, path), cookies=self.cookiejar)
 
     def _team_solves(self, team_id):
         r = self._get("/api/v1/{}/{}/solves".format(self.mode, team_id))
@@ -46,6 +49,14 @@ class CTFdScraper():
             }
         return stats
 
+    def _tasks(self):
+        logger.warning("getting tasks...")
+        r = self._get("/api/v1/challenges")
+        r.raise_for_status()
+        data = r.json()["data"]
+        logger.warning("done")
+        return [task["name"] for task in data]
+
     def scoreboard(self):
         logger.warning("getting scoreboard...")
         r = self._get("/api/v1/scoreboard")
@@ -53,7 +64,7 @@ class CTFdScraper():
         data = r.json()["data"]
         logger.warning("done")
 
-        tasks = set()
+        tasks = set(self._tasks())
         standings = []
         for team in tqdm(data):
             taskStats = self._team_solves(team["account_id"])
