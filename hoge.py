a824ad184bf18333ebb7c0d66c7a4a66e63ab334
theoremoon <theoldmoon0602@gmail.com>
2021-07-19 17:17:47 +0900

    Google CTF

---
+++ hoge.py
@@ -0,0 +1,35 @@
+ranking = ["perfect blue","Katzebin","DiceGang","PPP","Balsn","pasten","RedRocket","p4","Super Guesser","MoreSmokedLeetChicken","Tea Deliverers","WreckTheLine","🛹🐻","zer0pts","5BC","justCatTheFish","Kalmarunionen","PFS","vh++","Dragon Sector","r00timentary","hxp","thethetheduck","NeSE","Black Bauhinia","zwnj","Bushwhackers","TokyoWesterns","⚔️TSJ⚔️","Organizers","cr0wn","s3qu3nc3 ","C4T BuT S4D","Shellphish","ALLES!","r3kapig","FAUST","Vespiary","The Flat Network Society","FluxFingers","GatorSheavesMutably","room2042","KuK Hofhackerei","STT","Maple Bacon","dodododo","PLUS","the3000","CornfieldComputerCrew","Ph0t1n1a","DuckDuckNo","The Maccabees","Keyboard Monkeys","Tasteless","Syclover","r3billions","traicachli","greunion","ID-10-T","Recursively Descending The Deep Blue Suicide Mission","KUDoS","xtal","; TODO: Create team name","superflip","ARESx","IrisSec","brain damage","KITCTF","MikeCAT","bawolff","efiens","brkninjas","AntiBuddies","TheTeamless","better than team","SouthFlagsington","onepunchpwn","DarkArmy","DuckDuckFlag","gcmTime","idek","Lorem Ipsec","bi0s","SiBears","team_eat","matipau","RecursionFairies","srdnlen","Samurai","bootplug","slashbad","bit","Xp0int","andmed","anti-cult-cult","DCC","m4n3","KAIST GoN","EvilBunnyWrote","D33VOLUTIONS","No","curlyflower","potbelly","CzechCyberTeam","× × × × ×","Geeky Cat","ThereIsNoInfoRoma","0xCAFBE7","Nevsor","RPISEC","0","TheDefaced","ghidradarkmodewhen","F03v3ryY0ung","Scavenger Security","Saved by the Shell","cypis","ADDVulcan","ZipZalabim","dm0","JustDoIt","coffeelighten","B3NNY","dzil123","TEAM0001","Polymero","rfr","rabinkarp","Watch4Hack","SandiaN00bs","SSAT","ShamenDubon","CreateRemoteThread","DOUBLE STANDARD","wuf2rev","funtimes","Los Ninos","flk","A5537","Team'); DROP TABLE Teams;--","saarsec","shablul","Infinite loop","HugsForBugs","krzysztof1222","upbhack","ELT","🅱️EndREdi 🅱️aCArDi","zh3r0","mode13h","CyberOpenSquad","cyberwehr","mhackeroni","noraneco","Hungrycaterpillar","Team Enu","eskjekjaeba","灵雨安全实验室","bluecat","4919","Cache Money","dcua","temptemp","MirrorOS","OpenToAll","1t'5_n0t_fla9","XtraLemonz","YoungPwnawans","greenhorn","5upernova","MonkOnMars","nearkats","latiza","Saint Technics","Sector Z","Psyber Bois","junior_groot","PWr Synt@x Err0r","ET Bilu (seek knowledge)","Social Engineering Experts","Nomads","null","KogCoder","KUality","Hyp3rflow","0x62EEN7EA","PwN_3v3rY7h1nG","Applicative","K!nd4SUS","SuperNewbie","Tw1l1ght G4mb1t","adrian17","TangLangTech","memekatz","velvet_misconception","r0nd","EM_ANH_CHUONG_IDOL","Hackernator","ENOFLAG","apt593","InfosecIITG","CYBEANS","yult","OkabeRintaro","MuddySports","michiana-infosec","CTF-SG","ArcTropics","Shy♥Shachaf♥Shoshi","xiaobyeQQ","PECTF","Vidar Team","c0mp053r","Hopeful Raiders","NYUSEC","Can Haz Flag","gameofpwns","Lala5th","Blackjackets","RedDwarf","DIE","Bi0t_Ec","Brolerobot","SWAT","SKR","Harmony 4 You","SonOfSnowden","TWC","Jacoboom","Little Twoos","pmaster","C Flat","oakvedid","1dayPWNED","n0bitz","osmanthusrabbit","Wechall Warriors","Thundernerds","H4rdwo3k1ng","Secret Squirrels","WolvSec","PWD","BreakingCode","PolyHx","TorrescHack","bitb4ng","IITBreachers","YabbaDoo","AlbSociety","OnlyFeet","JufoUfo","ed","Belgian Red Daemons","peak^chaos","pingCTF","JustATeam","NoPwn4OldMen","<xufficiently 'STUP = id '>","xSTF","flagless","aaronpl","🐻🧮","AdelaideB9","NoTeamName","skiddies","curdir","FrenchRoomba","DeepThonts","CyKor","Cyber Erudites","CTFMON","R4nd0mGu3ssr","sponge_bob","m1t0","N30Z30N","❄️🌸","Hacklabor","CSALab_Sec","Apollo-13","burner_herz0g","4LEARN","MQCybersec","ev1111","kokeshi","Q&Z","lcgg","bnnz","wotop","_Marbin_","Billionaire_Space_Race","esar","4ever_1onely","Topological Surface","traP_casual","Pirates of the Caribbean","kad96","Ganesh","Yoo","shin2ro","RokirovkaDef","sf","ONEASTEAM","H3M5L0","chunankai","ByteForc3","Aceri","Team A","blue0620","Raise the Flag","Tucci Hang","keyball123","ArtAttHack","UglyDucklings","Handz","BitteNichtAnfuzzen!","Der Zimmermann","A!Tians","T0k","K17","Attack on Software","xXxpassword123xXx","Cybernated Wizards","SSL","0Trust","storm","upperVillage","Hacking to the Gate","TeamKrabath","Thehackerscrew","ktx","Throw another payload on the barbie","r00tstici","OctaC0re","Silver Catalysts","SaltyCabbages","KSP","Chimp","UW-Madison Cybersecurity Club","lángOS","Jump2Flag","mnsec","nbg","synackuk","ractor_burton","Zach","null2root","taln","n0w","toastedsteaksandwich","tabby_cat","13bd37","0r0","dickjustice228","Syntaxförster","SoloExploit","CLPEEE","EthicalCookieLovers","DeltaHeap","Last Placers","Hacklab","Beta_securiteam","Polaris One","greywolf","rdj","HuntsvilleTechSupport","nolax","hëdgehög","Eternal Deathslayer 3","Dababyboys","1337hackers","AnomalyZA","empty_jack","Cheese Wheel","MadrHacks","TheScientists","kks","Hacktimel","m0ck1ngbird","nukeshroom"]
+
+import json
+from datetime import datetime
+
+with open("hoge.json") as f:
+    scoreboard = json.load(f)
+
+tasks = set()
+standings  = []
+
+for teamname, v in scoreboard.items():
+    pos = ranking.index(teamname) + 1
+    taskStats = {}
+    for t, at in v["tasks"].items():
+        tasks.add(t)
+        taskStats[t] = {
+            "time": at,
+        }
+    standings.append({
+        "team": teamname,
+        "pos": pos,
+        "taskStats": taskStats,
+    })
+
+data = {
+    "tasks": list(tasks),
+    "standings": standings,
+    "date": int(datetime.strptime("2021-07-17", "%Y-%m-%d").timestamp()),
+    "name": "Google Capture The Flag 2021",
+}
+
+
+with open("./data/events/{}.json".format(data["name"].replace("/", "-")), "w") as f:
+    json.dump(data, f, indent=2, ensure_ascii=False)
